076b55371b0a18d3817af57b6627dcde4071c83d
Evian-Chen <paramecium128@gmail.com>
2025-03-08 15:30:50 +0800

    feat: add 796 python solution

---
+++ 796. Rotate String/796.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+
+        for _ in range(len(s)):
+            if s[0] not in goal: 
+                return False
+            new_s = s[1:] + s[0]
+            print(f"s: {s}, new_s: {new_s}")
+            
+            if goal == new_s:
+                return True
+            s = new_s
+        return False
